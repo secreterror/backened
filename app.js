d6b008fbee0d860cf95a607d4bef36a6d014369d
secreterror <yashgoenka100@gmail.com>
2021-08-08 16:11:07 +0530

    added dynamic port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/private',require('./routes/private'))
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT || 8000,()=>{
     console.log('listening')
 })
 
